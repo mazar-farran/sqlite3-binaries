d75e56d102cb81b36e0fae0c5052c1e5c0e8a33d
Fabio Spampinato <spampinabio@gmail.com>
2023-08-17 17:27:49 +0100

    Updated build

--- init/initshell.c
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2023 Anton Zhiyanov, MIT License
-// https://github.com/nalgeon/sqlite
-
-void shell_sqlean_init() {
-    sqlite3_snprintf(sizeof(mainPrompt), mainPrompt, "sqlean> ");
-}
--- init/initsqlite.c
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Anton Zhiyanov, MIT License
-// https://github.com/nalgeon/sqlite
-
-#include "init.h"
-
-int core_init(const char* dummy) {
-    shell_sqlean_init();
-    return sqlite3_auto_extension((void*)sqlite3_sqlean_init);
-}
